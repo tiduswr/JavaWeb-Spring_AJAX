c6e726c25c299f19f72ae22268454dfc8775210d
Harllem Nascimento <harllemnascimento@gmail.com>
2022-08-04 03:34:29 -0300

    First Commit(Configurações do APP)

---
+++ src/main/java/spring_ajax/SpringAjaxApplication.java
@@ -0,0 +1,13 @@
+package spring_ajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAjaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_ajax/SpringAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_ajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
